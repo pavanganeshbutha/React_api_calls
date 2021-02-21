653421577f671c0dbe15a07d092a9aa6a3ed2abb
Pavan Ganesh Butha <40636725+pavanganeshbutha@users.noreply.github.com>
2021-02-21 18:43:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [inputText, setInputText] = useState("");
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const URL = "https://pokeapi.co/api/v2/pokemon/" + inputText;
+  console.log(inputText);
+  console.log(URL);
+
+  function handleClick(event) {
+    event.preventDefault();
+    axios
+      .get(URL)
+      .then((response) => {
+        setName(response.data.name);
+        setImage(response.data.sprites.other.dream_world.front_default);
+        setInputText("");
+      })
+      .catch(() => {
+        setName("Pokemon not found");
+        setImage("");
+      });
+  }
+  return (
+    <div className="App">
+      <form>
+        <input
+          type="text"
+          placeholder="search"
+          value={inputText}
+          onChange={(event) => {
+            setInputText(event.target.value);
+          }}
+        />
+        <button type="submit" onClick={handleClick}>
+          search
+        </button>
+      </form>
+      <h1>{name}</h1>
+      <img src={image} alt="image" />
+    </div>
+  );
+}
